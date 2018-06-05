b6b473ebc97174fa9818059d47d87708993b467f
vasutomar <vasutomar26@gmail.com>
2018-06-06 00:01:00 +0530

    File creation and file check for user login credentials.

--- app/src/main/java/com/example/lenovo/passvault/CreateUserID.java
+++ app/src/main/java/com/example/lenovo/passvault/CreateUserID.java
@@ -1,13 +1,84 @@
 package com.example.lenovo.passvault;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CreateUserID extends AppCompatActivity {
 
+    private EditText usernameField;
+    private EditText passwordField;
+
+    private String userid;
+    private String userpass;
+
+    private final String File_Name = "LoginCredentials.txt";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user_id);
+
+        usernameField = (EditText)findViewById(R.id.Username);
+        passwordField = (EditText)findViewById(R.id.Password);
+
     }
+    public void MakeAccount(View v) throws IOException {
+
+        userid = usernameField.getText().toString();
+        userpass = passwordField.getText().toString();
+
+        String identifier1 = "Username:";
+        String identifier2 = "Password:";
+
+        try {
+            File file;
+            file = new File(File_Name);
+            FileOutputStream fos = null;
+            fos = openFileOutput(File_Name,Context.MODE_PRIVATE);
+            fos.write(identifier1.getBytes());
+            fos.write(userid.getBytes());
+            fos.write(identifier2.getBytes());
+            fos.write(userpass.getBytes());
+            fos.close();
+            String filepath = file.getAbsolutePath();
+            Toast.makeText(CreateUserID.this,filepath,Toast.LENGTH_LONG).show();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public void readingdata(View v) {
+        String Collected = null;
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(File_Name);
+            byte[] dataArray = new byte[fis.available()]; // returs amount of bytes file has
+            while(fis.read(dataArray)!=-1) {
+                Collected = new String(dataArray);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(CreateUserID.this,Collected,Toast.LENGTH_LONG).show();
+    }*/
 }
--- app/src/main/java/com/example/lenovo/passvault/MainActivity.java
+++ app/src/main/java/com/example/lenovo/passvault/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lenovo.passvault;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,29 +12,37 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String File_Name = "LoginCredentials.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    //EditText Username_field   = (EditText)findViewById(R.id.Username);
-    //EditText Password_field   = (EditText)findViewById(R.id.Password);
-
     public void Exit_Application(View v) {
         finish();
         System.exit(0);
     }
-
     public void Verify_User(View v) {
         Intent k = new Intent(this,LoginActivity.class);
         startActivity(k);
     }
-
     public void CreateUserId(View v) {
-        Intent k = new Intent(this,CreateUserID.class);
-        startActivity(k);
+        String path = "/LoginCredentials.txt";
+        File file = new File(path);
+        if(file.exists())
+            Toast.makeText(MainActivity.this,"Exists",Toast.LENGTH_LONG).show();
+        else{
+            Toast.makeText(MainActivity.this,"does not exist",Toast.LENGTH_LONG).show();
+            Intent k = new Intent(this,CreateUserID.class);
+            startActivity(k);
+        }
+
     }
 }
